afd75626467b467081b1b3b3dd7e425f68753785
Leticia-Aymee <leleaymee@gmail.com>
2026-04-21 14:17:38 -0300

    feat: implementando o componente MenuMobile e lógica de navegação responsiva

---
+++ src/components/MenuMobile.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+function MenuMobile({ isOpen, onClose } : any) {
+  return (
+    <div
+      className={`
+        fixed inset-0 w-full h-screen bg-[#211F1D]/95 backdrop-blur-md z-[100] 
+        transition-all duration-500 ease-in-out
+        ${isOpen ? "opacity-100 visible" : "opacity-0 invisible pointer-events-none"}
+      `}
+    >
+      {/* Topo Amarelo retangular colado no topo */}
+      <div className="w-full bg-[#FFD44B] h-[75px] flex items-center justify-between px-4 shadow-md">
+        <div className="flex items-center">
+          <img src="src/assets/icons/LogoBorda48.svg" alt="Logo" className="h-10 w-auto px-2" />
+          <p className="text-[#171717] font-bold text-lg">AAPM Senai Leopoldina</p>
+        </div>
+        <button onClick={onClose} className="text-[#1A1A1A] text-3xl font-light">
+          ✕
+        </button>
+      </div>
+
+      <nav className="flex flex-col h-[calc(100vh-75px)] px-10 pt-12 pb-10">
+        <div className="flex flex-col gap-6">
+          <Link to="/" onClick={onClose} className="text-[#CACACA] text-lg font-semibold border-b-2 border-[#FFF4C9] pb-2">
+            Home
+          </Link>
+          <Link to="/novidades" onClick={onClose} className="text-[#CACACA] text-lg font-semibold border-b-2 border-[#FFF4C9] pb-2">
+            Novidades
+          </Link>
+          <Link to="/pagamentos" onClick={onClose} className="text-[#CACACA] text-lg font-semibold border-b-2 border-[#FFF4C9] pb-2">
+            Pagamentos
+          </Link>
+        </div>
+
+        <div className="flex flex-col gap-4 mt-20">
+          <Link to="/login" onClick={onClose} className="w-full h-[45px] flex items-center justify-center rounded-full border border-white/30 text-[#CACACA] text-lg font-bold bg-[#171717]">
+            Entrar
+          </Link>
+          <Link to="/cadastro" onClick={onClose} className="w-full h-[45px] flex items-center justify-center rounded-full bg-[#FFD44B] text-[#171717] text-lg font-bold shadow-lg">
+            Cadastrar
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default MenuMobile;
