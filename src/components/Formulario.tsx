3bf85ef9ea9c7e917419b2ccd87743d275ecdb11
LETICIAOME <leticia.gomes@cptm.sp.gov.br>
2026-04-23 16:02:15 -0300

    feat: implementação da lógica de cadastro com validação de senha e tratamento de datas

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -2,21 +2,46 @@ import { useState } from "react";
 
 function Formulario({ tipo }) {
     const [usuario, setUsuario] = useState('aluno');
+    const [dados, setDados] = useState({ senha:'', confirmar_senha:'', dur_curso:'', data_inicio:'', data_final: ''});
+
+    const guardar = (e) => {
+        setDados({ ...dados, [e.target.name]: e.target.value });
+    }
+
+    const enviar = (e) => {
+        e.preventDefault();
+
+        if (tipo !== 'login' && dados.senha !== dados.confirmar_senha) {
+            alert("Senhas diferentes!");
+            return;
+        }
+
+        let dadosFinalizados = { ...dados };
+        if (usuario === 'aluno') {
+            dadosFinalizados.dur_curso = `${dados.data_inicio} até ${dados.data_final}`;
+
+            delete dadosFinalizados.data_inicio;
+            delete dadosFinalizados.data_final;
+        }
+        delete dadosFinalizados.confirmar_senha;
+
+        console.log("Enviando:", dadosFinalizados);
+    }
 
     return (
-        <form>
+        <form onSubmit={enviar}>
             <h2>{tipo === 'login' ? 'Faça seu Login' : 'Faça seu Cadastro'}</h2>
 
             {tipo !== 'login' && (
                 <div>
                     <label>Nome:</label>
-                    <input type="text" name="nome" />
+                    <input type="text" name="nome" onChange={guardar} />
                 </div>
             )}
 
             <div>
                 <label>Email:</label>
-                <input type="email" name="email"/>
+                <input type="email" name="email" onChange={guardar}/>
             </div>
 
             {tipo !== 'login' && (
@@ -33,7 +58,7 @@ function Formulario({ tipo }) {
                         <>
                             <div>
                                 <label>Curso:</label>
-                                <input type="text" name="curso" />
+                                <input type="text" name="curso" onChange={guardar} />
                             </div> 
 
                             <div>
@@ -41,12 +66,12 @@ function Formulario({ tipo }) {
                                 
                                 <div>
                                     <label>Data Inicio</label>
-                                    <input type="date" name="data_inicio" />
+                                    <input type="date" name="data_inicio" onChange={guardar} />
                                 </div>
 
                                 <div>
                                     <label>Data Final</label>
-                                    <input type="date" name="data_final" />
+                                    <input type="date" name="data_final" onChange={guardar} />
                                 </div>
                             </div> 
  
@@ -54,7 +79,7 @@ function Formulario({ tipo }) {
                     ) : (
                         <div>
                             <label>Especialidade:</label>
-                            <input type="text" name="especialidade" />
+                            <input type="text" name="especialidade" onChange={guardar}/>
                         </div>
                     )}
                 </>
@@ -62,13 +87,13 @@ function Formulario({ tipo }) {
 
             <div>
                 <label>Senha:</label>
-                <input type="password" name="senha" />
+                <input type="password" name="senha" onChange={guardar}/>
             </div>
 
             {tipo !== 'login' && (
                 <div>
                     <label>Confirmar Senha:</label>
-                    <input type="password" name="confirmar_senha" />
+                    <input type="password" name="confirmar_senha" onChange={guardar}/>
                 </div>
             )}
 
