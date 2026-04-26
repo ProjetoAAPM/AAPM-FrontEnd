31236af05ece3b1dbc01bb47765a6dff385a8835
Flavia Ribeiro <flavinasribs@gmail.com>
2026-04-26 07:22:15 -0300

    feat: Criar  uma conexão entre o Header e o restante da página. Agora, quando você clica em 'Editar', a página toda entende o comando e libera as caixas de edição nos textos e imagens.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,23 +1,67 @@
 import Inicio from "../components/landingPage/Inicio";
 import SobreNos from "../components/landingPage/SobreNos";
 import Planos from "../components/landingPage/Planos";
+import Header from "../components/Header";
+import { useState } from "react";
 
-function LandingPage() {
-    return (
-        <div>
-            <section id="inicio">
-                <Inicio />
-            </section>
+function LandingPage({ isAdmin = false }: any) {
+  const [isEditing, setIsEditing] = useState(false);
 
-            <section id="sobre" className=" bg-[#101625] min-h-screen">
-                <SobreNos />
-            </section>
+  const [content, setContent] = useState({
+    hero: {
+      html: `
+        <h1>
+          Conectando alunos,<br/>
+          escola <span class='text-[#FFDB4B]'>e comunidade</span>
+        </h1>
+        <p>
+          Faça parte de projetos que transformam o Senai em um lugar ainda melhor para aprender e crescer
+        </p>
+        <p>
+          Pequenas ações que melhoram a vida do aluno
+        </p>
+      `
+    },
+    sobre: {
+      origem: "Surgimos tendo em vista a necessidade de criar uma relação mais próxima entre a escola técnica e a comunidade local...",
+      apoio1: "Suporte financeiro, empréstimo de armários, e acesso a ferramentas de lazer e esportes.",
+      apoio2: "Eventos, formaturas, feiras de tecnologia e confraternizações.",
+    }
+  });
 
-            <section id="planos" className="bg-[#101625] min-h-screen">
-                <Planos />
-            </section>
-        </div>
-    );
+  return (
+    <>
+      <Header
+        isAdmin={isAdmin}
+        isEditing={isEditing}
+        toggleEditing={() => setIsEditing(!isEditing)}
+      />
+
+      <div>
+        <section id="inicio">
+          <Inicio
+            isAdmin={isAdmin}
+            isEditing={isEditing}
+            content={content}
+            setContent={setContent}
+          />
+        </section>
+
+        <section id="sobre" className="bg-[#101625] min-h-screen">
+          <SobreNos
+            isAdmin={isAdmin}
+            isEditing={isEditing}
+            content={content}
+            setContent={setContent}
+          />
+        </section>
+
+        <section id="planos" className="bg-[#101625] min-h-screen">
+          <Planos />
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
