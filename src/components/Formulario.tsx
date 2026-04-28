9f858f6e5cb2320ff92dd1dfada279cfbb315f50
Leticia-Aymee <leleaymee@gmail.com>
2026-04-28 09:24:32 -0300

    refactor: extração de estilos da label e input para variáveis dinâmicas

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -28,7 +28,7 @@ function Formulario({ tipo } : any) {
         console.log("Enviando:", dadosFinalizados);
     }
 
-     const cores = {
+    const cores = {
         aluno: { bg: 'bg-[#C83D3D]', bg_label: 'bg-[#902C2D]', btn: 'bg-[#383636]', titulo: 'text-[#FFFFFF]' },
         docente: { bg: 'bg-[#86D5FE]', bg_label: 'bg-[#2C6090]', btn: 'bg-[#383636]', titulo: 'text-[#16334D]' },
         login: { bg: 'bg-[#FFEFAF]', bg_label: 'bg-[#FFDB4B]', btn: 'bg-[#FFDB4B]', titulo: 'text-[#101625]' }
@@ -36,56 +36,60 @@ function Formulario({ tipo } : any) {
 
     const tema = tipo === 'login' ? cores.login : (usuario === 'aluno' ? cores.aluno : cores.docente);
 
+    const estiloLabel = `${tema.bg_label} w-fit py-2 px-14 rounded-r-full mb-2 text-lg inset-shadow-sm inset-shadow-indigo-700/10 ${tipo === 'login' ? ' text-[#373737] font-bold' : 'pl-20  -ml-18.5 text-[#FFFFFF] font-semibold' }`;
+
+    const estiloInput = `w-full h-[40px] p-1 bg-white rounded-md ${tipo === 'login' ? 'mx-15 shadow-md' : 'mx-2 shadow-md'}`;
+
     return (
         <div className="w-full flex justify-center py-20">
             <form onSubmit={enviar} className={`${tema.bg} ${tipo === 'login' ? 'min-h-[500px] w-[500px]' : 'max-w-[848px] w-full min-h-screen'} mx-auto py-10 rounded-lg flex flex-col items-center gap-6`}>
                 <img src="src/assets/icons/Logo48.svg" alt="logo" className="absolute top-[30px] h-[100px] w-auto drop-shadow-md"/>
-                <h2 className="text-4xl font-bold italic text-white mt-6">
+                <h2 className={`text-4xl font-bold italic mt-4 ${tema.titulo}`}>
                     {tipo === 'login' ? 'Faça seu Login' : 'Faça seu Cadastro'}
                 </h2>
 
                 <div className="w-full max-w-[700px] flex flex-col gap-8">
                     {tipo !== 'login' && (
                         <div className="flex flex-col w-full">
-                            <label className="bg-[#902C2D] w-fit py-2 px-14 pl-20 -ml-18.5 rounded-r-full mb-2 text-white text-lg font-semibold ">Nome:</label>
-                            <input type="text" name="nome" onChange={guardar} className="w-full h-[40px] p-1 bg-white rounded-md shadow-md" />
+                            <label className={estiloLabel}>Nome:</label>
+                            <input type="text" name="nome" onChange={guardar} className={estiloInput} />
                         </div>
                     )}
 
                     <div className="flex flex-col">
-                        <label className="bg-[#902C2D] w-fit py-2 px-15 pl-20 -ml-18.5 rounded-r-full mb-2 text-white text-lg font-semibold">Email:</label>
-                        <input type="email" name="email" onChange={guardar} className="w-full h-[40px] p-1 bg-white rounded-md shadow-md"/>
+                        <label className={estiloLabel}>E-mail:</label>
+                        <input type="email" name="email" onChange={guardar} className={estiloInput}/>
                     </div>
 
                     {tipo !== 'login' && (
                         <>
                             <div className="flex flex-col">
-                                <label className="bg-[#902C2D] w-fit py-2 px-12 pl-20 -ml-18.5 rounded-r-full mb-2 text-white text-lg font-semibold">Você é?</label>
+                                <label className={estiloLabel}>Você é?</label>
                                 <div className="grid grid-cols-5 gap-6">
-                                    <button type="button" onClick={() => setUsuario('aluno')} className="w-[130px] h-[45px] bg-[#383636] rounded-xl text-white text-lg font-bold shadow-sm cursor-pointer">Aluno</button>
-                                    <button type="button" onClick={() => setUsuario('docente')} className="w-[130px] h-[45px] bg-[#DDDDDD] rounded-xl text-black text-lg font-bold shadow-sm cursor-pointer">Docente</button>
+                                    <button type="button" onClick={() => setUsuario('aluno')} className={`w-[130px] h-[45px] rounded-xl text-lg font-bold shadow-sm cursor-pointer ${usuario === 'aluno' ? 'bg-[#383636] text-white ' : 'bg-[#DDDDDD] text-black'}`}>Aluno</button>
+                                    <button type="button" onClick={() => setUsuario('docente')} className={`w-[130px] h-[45px] rounded-xl text-lg font-bold shadow-sm cursor-pointer ${usuario === 'docente' ? 'bg-[#383636] text-white ' : 'bg-[#DDDDDD] text-black'}`}>Docente</button>
                                 </div>
                             </div>
 
                             {usuario === 'aluno' ? (
                                 <>
                                     <div className="flex flex-col">
-                                        <label className="bg-[#902C2D] w-fit py-2 px-15 pl-20 -ml-18.5 rounded-r-full mb-2 text-white text-lg font-semibold">Curso:</label>
-                                        <input type="text" name="curso" onChange={guardar} className="w-full h-[40px] p-1 bg-white rounded-md shadow-md"/>
+                                        <label className={estiloLabel}>Curso:</label>
+                                        <input type="text" name="curso" onChange={guardar} className={estiloInput}/>
                                     </div> 
 
                                     <div className=" flex flex-col">
-                                        <label className="bg-[#902C2D] w-fit py-2 px-15 pl-20 -ml-18.5 rounded-r-full mb-2 text-white text-lg font-semibold">Duração do Curso:</label>
+                                        <label className={estiloLabel}>Duração do Curso:</label>
                                         
                                         <div className="grid grid-cols-4 gap-4">
                                             <div className="flex flex-col">
                                                 <label className="text-white text-lg text-center font-semibold mb-2">Data Inicio</label>
-                                                <input type="date" name="data_inicio" onChange={guardar} className="w-full h-[40px] p-4 bg-white rounded-md shadow-sm"/>
+                                                <input type="date" name="data_inicio" onChange={guardar} className={estiloInput}/>
                                             </div>
 
                                             <div className="flex flex-col">
                                                 <label className="text-white text-lg text-center font-semibold mb-2">Data Final</label>
-                                                <input type="date" name="data_final" onChange={guardar} className="w-full h-[40px] p-4 bg-white rounded-md shadow-sm"/>
+                                                <input type="date" name="data_final" onChange={guardar} className={estiloInput}/>
                                             </div>
                                         </div>
                                     </div> 
@@ -93,27 +97,27 @@ function Formulario({ tipo } : any) {
                                 </>
                             ) : (
                                 <div className="flex flex-col">
-                                    <label className="bg-[#902C2D] w-fit py-2 px-15 pl-20 -ml-18.5 rounded-r-full mb-2 text-white text-lg font-semibold">Especialidade:</label>
-                                    <input type="text" name="especialidade" onChange={guardar} className="w-full h-[40px] p-1 bg-white rounded-md shadow-md"/>
+                                    <label className={estiloLabel}>Especialidade:</label>
+                                    <input type="text" name="especialidade" onChange={guardar} className={estiloInput}/>
                                 </div>
                             )}
                         </>
                     )}
 
                     <div className="flex flex-col">
-                        <label className="bg-[#902C2D] w-fit py-2 px-15 pl-20 -ml-18.5 rounded-r-full mb-2 text-white text-lg font-semibold">Senha:</label>
-                        <input type="password" name="senha" onChange={guardar} className="w-full h-[40px] bg-white rounded-md shadow-md"/>
+                        <label className={estiloLabel}>Senha:</label>
+                        <input type="password" name="senha" onChange={guardar} className={estiloInput}/>
                     </div>
 
                     {tipo !== 'login' && (
                         <div className="flex flex-col">
-                            <label className="bg-[#902C2D] w-fit py-2 px-15 pl-20 -ml-18.5 rounded-r-full mb-2 text-white text-lg font-semibold">Confirmar Senha:</label>
-                            <input type="password" name="confirmar_senha" onChange={guardar} className="w-full h-[40px] bg-white rounded-md shadow-md"/>
+                            <label className={estiloLabel}>Confirmar Senha:</label>
+                            <input type="password" name="confirmar_senha" onChange={guardar} className={estiloInput}/>
                         </div>
                     )}
 
                     <div className="flex justify-center mt-4">
-                        <button type="submit" className="w-[170px] h-[50px] bg-[#383636] rounded-2xl text-white text-xl font-bold shadow-md cursor-pointer" >{tipo === 'login' ? 'Entrar' : 'Cadastrar-se'}</button>
+                        <button type="submit" className={`${tema.btn} ${tipo === 'login' ? 'w-[170px] h-[50px] rounded-full text-[#373737]' : 'w-[170px] h-[50px] rounded-2xl text-[#FFFFFF]'} text-xl font-bold shadow-md cursor-pointer`} >{tipo === 'login' ? 'Entrar' : 'Cadastrar-se'}</button>
                     </div>
 
                 </div>
