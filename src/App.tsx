79a8e738354ce3cb9d799461a432e26cda5ad4e4
Leticia-Aymee <leleaymee@gmail.com>
2026-04-28 09:03:16 -0300

    fix: excluindo importação do BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import LandingPage from "./pages/LandingPage";
