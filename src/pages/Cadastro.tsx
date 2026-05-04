7905a4dde0a29ffe78558bbe0c90db50592f99ed
Leticia-Aymee <leleaymee@gmail.com>
2026-05-04 10:34:38 -0300

    fix: alteração de altura fixa para altura mínima para evitar corte de conteúdo

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -2,7 +2,7 @@ import Formulario from "../components/Formulario";
 
 function Cadastro() {
     return (
-        <div className="bg-[#101625] h-[100vh]">
+        <div className="bg-[#101625] min-h-[100vh]">
             <img src="src/assets/detalheEstilo.png" alt="bolinhas" className="absolute top-0 left-0 z-0 opacity-80 w-[350px]" />
             <img src="src/assets/detalheEstilo.png" alt="bolinhas" className="absolute bottom-0 right-0 z-0 opacity-80 w-[300px] -scale-x-100 -scale-y-100" />
 
