2a2dd5873f3663a3cb8a48a851408f987ab548d8
Leticia-Aymee <leleaymee@.com>
2026-05-05 14:57:01 -0300

    refactor: padronizar CardPlano utilizando interface e children para melhor flexibilidade

--- src/components/CardPlano.tsx
+++ src/components/CardPlano.tsx
@@ -1,7 +1,18 @@
 import { Star } from "lucide-react";
-import { Link } from "react-router-dom";
+import type React from "react";
 
-function CardPlano({ fundoTitulo, titulo, preco, tempo, corTopo, corBtn, corBorda, popular, children, esconderBotao } : any) {
+interface CardProps {
+    titulo: string;
+    fundoTitulo: string;
+    className?: string;
+    popular?: boolean;
+    textoBtn?: string;
+    onClick?: () => void;
+    children: React.ReactNode;
+}
+
+function CardPlano({ titulo, fundoTitulo, className, popular, textoBtn, onClick, children } : CardProps) {
+
     return (
         <div className={`relative w-full max-w-[435px] h-auto lg:min-h-[619px] mt-10 ${popular ? 'scale-100 lg:scale-105' : ''}`}>
             
@@ -10,7 +21,7 @@ function CardPlano({ fundoTitulo, titulo, preco, tempo, corTopo, corBtn, corBord
             )}
 
             <div className={`w-full h-full lg:min-h-[619px] flex flex-col items-center text-center shadow-lg relative z-10 border-4 rounded-2xl
-                ${popular ? `bg-linear-to-tl from-[#F3DD89] via-[#F6E7AB] to-[#FFF9E0] ${corBorda}` : `bg-white ${corBorda}`}`}>
+                ${className}`}>
 
                 {popular && (
                     <div className="absolute w-[90%] max-w-[296px] h-[52px] -top-6 left-1/2 -translate-x-1/2 bg-linear-to-r from-[#F1D052] via-[#FFEFAF] to-[#F0C72B] rounded-full shadow-md flex justify-center items-center gap-2 z-20">
@@ -19,28 +30,21 @@ function CardPlano({ fundoTitulo, titulo, preco, tempo, corTopo, corBtn, corBord
                     </div>
                 )}
 
-                <div className={`${fundoTitulo} w-full h-[86px] py-px rounded-t-xl mb-4 ${popular ? 'h-32 pt-5' : ''}`}>
+                <div className={`${fundoTitulo} flex items-center justify-center mb-4 `}>
                     <h3 className="text-2xl md:text-3xl font-bold mt-4 mb-4 text-black italic">
                         {titulo}
                     </h3>
                 </div>
 
-                <div className={`${corTopo} w-full h-auto min-h-[47px] py-2 font-bold italic text-2xl md:text-3xl mt-4 mb-6`}>
-                    {preco} <span className="text-xl md:text-2xl font-normal">{tempo}</span>
-                </div>
-
-                <div className="px-6 md:px-8 pb-8 flex flex-col h-full grow w-full">
-                    <div className="flex-1">
+                
+                <div className="px-6 md:px-8 flex flex-col grow w-full">
+                    <div className="flex-1 flex flex-col items-center justify-center">
                         {children}
                     </div>
 
-                    {!esconderBotao && (
-                        <Link to="/cadastro" className="mt-6 mb-4">
-                            <button className={`${corBtn} w-[224px] max-w-full h-[54px] px-6 py-2 rounded-lg font-bold shadow-md text-black text-xl md:text-2xl cursor-pointer transition-all duration-300 hover:opacity-85 hover:scale-105 active:scale-95`}>
-                                Contribuir
-                            </button>
-                        </Link>
-                    )}
+                    <button>
+                        { textoBtn || "Contribuir"}
+                    </button>
                 </div>
             </div>
         </div>
