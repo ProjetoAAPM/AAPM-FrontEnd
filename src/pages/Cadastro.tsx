e4a53e118eb9ba988ec949a2b128453b439d39c7
Leticia-Aymee <leleaymee@gmail.com>
2026-05-13 16:51:51 -0300

    feat: adicionando botão de retorno nas páginas de login e cadastro

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -1,11 +1,21 @@
 import Formulario from "../components/Formulario";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
 function Cadastro() {
+    const navigate = useNavigate();
+
     return (
         <div className="bg-[#101625] min-h-screen flex flex-col items-center relative pb-20">
             <img src="src/assets/detalheEstilo.png" alt="bolinhas" className="absolute top-0 left-0 z-0 opacity-80 w-[350px]" />
             <img src="src/assets/detalheEstilo.png" alt="bolinhas" className="absolute bottom-0 right-0 z-0 opacity-80 w-[300px] -scale-x-100 -scale-y-100" />
+
+            <button
+                onClick={() => navigate('/')}
+                className="absolute top-10 left-15 flex items-center gap-2 cursor-pointer"
+            >
+                <ArrowLeft size={30} color="#FFFFFF" strokeWidth={3}/>
+            </button>
 
             <Formulario tipo="cadastro" className="-mt-20" />
 
