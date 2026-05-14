77adf46c691c18c385497235fc6ac84127c2458e
MAYSA SOARES DOS SANTOS <maysa.santos@cptm.sp.gov.br>
2026-05-14 16:52:39 -0300

    feat: Criação da edição do ModalPerfil e fotos de perfil

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,21 +7,115 @@ function Home() {
   const [extrato, setExtrato] = useState([]);
 
   return (
-    <div className="bg-[#101625]">
+    <div className="bg-[#101625] w-full overflow-x-hidden">
+      
+      <section
+        className="
+          min-h-screen
+          flex
+          items-center
+          justify-center
 
-      <section className="min-h-[100vh] flex items-center justify-center pt-24">
+          pt-16
+          px-2
+
+          min-[375px]:pt-20
+          min-[375px]:px-3
+
+          sm:pt-24
+          sm:px-4
+
+          md:pt-28
+          md:px-6
+
+          lg:pt-32
+          lg:px-8
+
+          xl:pt-24
+          xl:px-10
+
+          2xl:pt-24
+          2xl:px-10
+        "
+      >
         <Pontuacao setExtrato={setExtrato} />
       </section>
 
-      <section className="min-h-[90vh] flex items-center justify-center">
+      <section
+        className="
+          min-h-[70vh]
+          flex
+          items-center
+          justify-center
+
+          px-2
+          py-8
+
+          min-[375px]:px-3
+          min-[375px]:py-10
+
+          sm:min-h-[75vh]
+          sm:px-4
+          sm:py-12
+
+          md:min-h-[80vh]
+          md:px-6
+          md:py-14
+
+          lg:min-h-[85vh]
+          lg:px-8
+          lg:py-16
+
+          xl:min-h-[90vh]
+          xl:px-10
+          xl:py-20
+
+          2xl:min-h-[90vh]
+          2xl:px-10
+          2xl:py-20
+        "
+      >
         <FormularioExtrato extrato={extrato} />
       </section>
 
-      <section className="min-h-[90vh] flex items-center justify-center">
+      <section
+        className="
+          min-h-[70vh]
+          flex
+          items-center
+          justify-center
+
+          px-2
+          pb-8
+
+          min-[375px]:px-3
+          min-[375px]:pb-10
+
+          sm:min-h-[75vh]
+          sm:px-4
+          sm:pb-12
+
+          md:min-h-[80vh]
+          md:px-6
+          md:pb-14
+
+          lg:min-h-[85vh]
+          lg:px-8
+          lg:pb-16
+
+          xl:min-h-[90vh]
+          xl:px-10
+          xl:pb-20
+
+          2xl:min-h-[90vh]
+          2xl:px-5
+        "
+      >
         <Sugestoes />
       </section>
 
     </div>
   );
 }
+
 export default Home;
