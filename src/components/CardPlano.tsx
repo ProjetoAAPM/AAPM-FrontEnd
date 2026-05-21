442ec3d4d95f8f52086f2b0f46c1ae2aafaabd14
Leticia-Aymee <leleaymee@gmail.com>
2026-05-21 14:24:32 -0300

    fix: corrige erros de sintaxe e comportamento após atualização

--- src/components/CardPlano.tsx
+++ src/components/CardPlano.tsx
@@ -1,46 +1,51 @@
 import { Star } from "lucide-react";
-import { Link } from "react-router-dom";
+import type React from "react";
 
-function CardPlano({ fundoTitulo, titulo, preco, tempo, corTopo, corBtn, corBorda, popular, children, esconderBotao } : any) {
+interface CardProps {
+    titulo: string;
+    fundoTitulo: string;
+    className?: string;
+    popular?: boolean;
+    textoBtn?: string;
+    corBtn: string;
+    onClick?: () => void;
+    children: React.ReactNode;
+}
+
+function CardPlano({ titulo, fundoTitulo, className, popular, textoBtn, corBtn, onClick, children } : CardProps) {
+
     return (
-        <div className={`relative w-full max-w-[435px] h-auto lg:min-h-[619px] mt-10 ${popular ? 'scale-100 lg:scale-105' : ''}`}>
+        <div className={`relative w-[270px] md:w-[340px] lg:w-[380px] xl:w-[435px] h-auto mt-10 ${popular ? 'scale-100 lg:scale-105' : ''}`}>
             
             {popular && (
                 <div className="absolute inset-0 bg-[#F1D052] rounded-3xl blur-3xl animate-pulse opacity-80 z-0"></div>
             )}
 
-            <div className={`w-full h-full lg:min-h-[619px] flex flex-col items-center text-center shadow-lg relative z-10 border-4 rounded-2xl
-                ${popular ? `bg-linear-to-tl from-[#F3DD89] via-[#F6E7AB] to-[#FFF9E0] ${corBorda}` : `bg-white ${corBorda}`}`}>
+            <div className={`w-full h-full lg:min-h-[619px] flex flex-col items-center text-center shadow-lg relative z-10 rounded-2xl
+                ${className}`}>
 
                 {popular && (
-                    <div className="absolute w-[90%] max-w-[296px] h-[52px] -top-6 left-1/2 -translate-x-1/2 bg-linear-to-r from-[#F1D052] via-[#FFEFAF] to-[#F0C72B] rounded-full shadow-md flex justify-center items-center gap-2 z-20">
+                    <div className="absolute w-[90%] max-w-[200px] md:max-w-[270px] h-[35px] md:h-[45px] lg:h-[50px] -top-6 left-1/2 -translate-x-1/2 bg-linear-to-r from-[#F1D052] via-[#FFEFAF] to-[#F0C72B] rounded-full shadow-md flex justify-center items-center gap-2 z-20">
                         <Star size={24} className="text-[#BE9700]" fill="currentColor"/>
-                        <h3 className="text-xl md:text-2xl font-bold text-[#332900]">MAIS POPULAR</h3>
+                        <h3 className="text-base md:text-xl font-bold text-[#332900]">MAIS POPULAR</h3>
                     </div>
                 )}
 
-                <div className={`${fundoTitulo} w-full h-[86px] py-px rounded-t-xl mb-4 ${popular ? 'h-32 pt-5' : ''}`}>
-                    <h3 className="text-2xl md:text-3xl font-bold mt-4 mb-4 text-black italic">
+                <div className={`${fundoTitulo} flex items-center justify-center mb-4 `}>
+                    <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mt-2 mb-2 md:mt-4 md:mb-4 text-black">
                         {titulo}
                     </h3>
                 </div>
 
-                <div className={`${corTopo} w-full h-auto min-h-[47px] py-2 font-bold italic text-2xl md:text-3xl mt-4 mb-6`}>
-                    {preco} <span className="text-xl md:text-2xl font-normal">{tempo}</span>
-                </div>
-
-                <div className="px-6 md:px-8 pb-8 flex flex-col h-full grow w-full">
-                    <div className="flex-1">
+                
+                <div className=" flex flex-col grow w-full">
+                    <div className="flex-1 flex flex-col items-center justify-center">
                         {children}
                     </div>
 
-                    {!esconderBotao && (
-                        <Link to="/cadastro" className="mt-6 mb-4">
-                            <button className={`${corBtn} w-[224px] max-w-full h-[54px] px-6 py-2 rounded-lg font-bold shadow-md text-black text-xl md:text-2xl cursor-pointer transition-all duration-300 hover:opacity-85 hover:scale-105 active:scale-95`}>
-                                Contribuir
-                            </button>
-                        </Link>
-                    )}
+                    <button onClick={onClick} className={`${corBtn} w-[110px] h-[32px] md:w-[150px] md:h-[44px] lg:w-[180px] lg:h-[54px] mx-auto rounded-md md:rounded-lg lg:rounded-xl font-bold shadow-md hover:scale-105 active:scale-95 transtion-all mt-5 mb-4 text-lg md:text-xl lg:text-2xl cursor-pointer`}>
+                        { textoBtn || "Contribuir"}
+                    </button>
                 </div>
             </div>
         </div>
