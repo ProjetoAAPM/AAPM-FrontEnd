572c6a2960df4685e34db089f8d648006d35c5da
Leticia-Aymee <leleaymee@gmail.com>
2026-05-26 16:10:30 -0300

    style: aplica classes responsivas de gap, margem e tamanho de fonte nos cards

--- src/pages/Pagamento.tsx
+++ src/pages/Pagamento.tsx
@@ -14,17 +14,17 @@ function Pagamento() {
     return (
         <div className="min-h-[100vh] bg-[#101625] flex flex-col items-center py-5">
 
-            <div className="flex flex-wrap justify-center gap-20 w-full mb-15 mt-37 justify-between">
+            <div className="flex flex-wrap justify-center gap-3 md:gap-10 lg:gap-20 w-full mb-15 mt-23 md:mt-25 lg:mt-37 justify-between">
                 <CardPlano
                     titulo="Plano Comum"
                     className="bg-[#FFFFFF] border-3 border-[#86D5FE]"
                     fundoTitulo="bg-[#86D5FE] w-full md:h-[60px] lg:h-[80px] rounded-t-xl italic"
                     popular={false}
                     textoBtn="Pagar"
-                    corBtn="bg-[#86D5FE] italic -mt-10 mb-10"
+                    corBtn="bg-[#86D5FE] italic lg:-mt-10 lg:mb-10"
                     onClick={() => handlPrepararEnvio("comum")} 
                 >
-                    <p className="bg-[#86D5FE]/80 rounded-md p-6 mx-6 text-base md:text-xl lg:text-[22px] -mt-32"> 
+                    <p className="bg-[#86D5FE]/80 rounded-md p-6 mx-6 lg:-mt-32 text-sm md:text-xl lg:text-[22px]"> 
                         Esse plano permite acompanhar as iniciativas da plataforma, participar das atividades disponíveis e acumular pontos que podem ser utilizados para conquistar brindes e recompensas ao longo do período.
                     </p>
                 </CardPlano>
@@ -35,10 +35,10 @@ function Pagamento() {
                     fundoTitulo="bg-linear-to-r from-[#F1D052] via-[#F0C72B] to-[#EFBF04] w-full md:h-[70px] lg:h-[90px] rounded-t-xl italic"
                     popular={true}
                     textoBtn="Pagar"
-                    corBtn="bg-linear-to-r from-[#F1D052] via-[#F0C72B] to-[#EFBF04] italic mb-13"
+                    corBtn="bg-linear-to-r from-[#F1D052] via-[#F0C72B] to-[#EFBF04] italic lg:mb-13"
                     onClick={() => handlPrepararEnvio("premium")}   
                 >
-                    <p className="bg-[#EFC10E]/80 rounded-md p-4 mx-6 text-base md:text-xl lg:text-[22px] -mt-5"> 
+                    <p className="bg-[#EFC10E]/80 rounded-md p-4 mx-6 lg:-mt-2 text-sm md:text-xl lg:text-[22px] "> 
                         O Plano Premium oferece vantagens exclusivas que permitem acumular pontos mais rapidamente e acessar recompensas diferenciadas. Além disso, usuários premium recebem maior destaque no sistema de gamificação e benefícios especiais em campanhas e eventos.
                     </p>
                 </CardPlano>
@@ -49,7 +49,7 @@ function Pagamento() {
                     fundoTitulo="bg-[#C83D3D] w-full md:h-[60px] lg:h-[80px] rounded-t-xl italic"
                     popular={false}
                     textoBtn="Turbinar!"
-                    corBtn="bg-[#C83D3D] italic mb-10"
+                    corBtn="bg-[#C83D3D] italic lg:mb-10"
                     onClick={() => handlPrepararEnvio("turbinar")}
                 >
                     <div className="bg-[#C83D3D]/80 w-full lg:h-[40px] 
@@ -60,7 +60,7 @@ function Pagamento() {
                         >
                             R$80 
                         <span className="md:text-[20px] lg:text-[22px] font-medium">/ ativação</span></div>
-                    <p className="bg-[#C83D3D]/80 mt-4 rounded-md p-4 mx-6 text-base md:text-xl lg:text-[22px] -mt-5">
+                    <p className="bg-[#C83D3D]/80 mt-4 lg:-mt-3 rounded-md p-4 mx-6 text-sm md:text-xl lg:text-[22px] ">
                         Essa opção permite aumentar sua pontuação rapidamente, ajudando a desbloquear brindes e recompensas disponíveis no sistema. Ao ativá-la, o usuário recebe um reforço em seus pontos acumulados, acelerando seu progresso na plataforma.
                     </p>
                 </CardPlano>
