f2798b6d1acef09866924919b4e843870fa76ac2
Leticia-Aymee <leleaymee@gmail.com>
2026-05-26 05:12:33 -0300

    style: torna logo, títulos, cards e fontes responsivos em telas menores

--- src/pages/EscolhaPlano.tsx
+++ src/pages/EscolhaPlano.tsx
@@ -111,37 +111,37 @@ function EscolhaPlano() {
 
            <div className="w-full max-w-5xl mb-5">
                 <div className="flex justify-center mb-30">
-                     <img src="src/assets/icons/Logo48.svg" alt="logo" className="absolute h-[100px] w-auto drop-shadow-md"/>
+                     <img src="src/assets/icons/Logo48.svg" alt="logo" className="absolute  lg:-mt-0.5 h-[70px] md:h-[80px] lg:h-[100px] w-auto drop-shadow-md"/>
                 </div>
-                <div className="w-screen relative left-1/2 right-1/2 -ml-[50vw] bg-[#42B9F4] py-4 mb-3 h-[80px]">
-                    <h1 className="text-white text-center text-4xl font-bold italic">
+                <div className="w-screen relative left-1/2 right-1/2 -ml-[50vw] bg-[#42B9F4] py-4 -mt-8 -mb-4 md:-mt-5 md:mb-2 lg:-mt-1 lg:mb-3 lg:h-[80px]">
+                    <h1 className="text-white text-center text-2xl md:text-3xl lg:text-4xl font-bold italic">
                         Escolha o seu plano
                     </h1>
                 </div>
            </div>
 
-           <div className="flex flex-wrap justify-center gap-8 w-full mb-15 justify-between">
+           <div className="flex flex-wrap justify-center gap-4 md:gap-6 lg:gap-8 w-full mb-15 justify-between">
                 <CardPlano
                     titulo="Plano Comum"
                     className="bg-[#86D5FE]"
-                    fundoTitulo="bg-white mr-10.5 mt-10 w-[90%] h-[40px] rounded-r-xs"
+                    fundoTitulo="bg-white mr-10 mt-8 md:mr-12.5 md:mt-9 lg:mr-10.5 lg:mt-10 w-[85%] lg:w-[90%] h-[40px] rounded-r-xs"
                     popular={false}
                     textoBtn="Upload"
                     corBtn="bg-[#373737] text-white"
                     onClick={() => handlPrepararEnvio("comum", comprovanteComum)}
                 > 
-                    <img src="src/assets/images/qrcode.png" alt="qrcode" className="w-[120px] h-[120px] md:w-[187px] md:h-[187px] rounded-2xl " />
+                    <img src="src/assets/images/qrcode.png" alt="qrcode" className="w-[110px] h-[110px] md:w-[150px] md:h-[150px] lg:w-[187px] lg:h-[187px] rounded-2xl " />
                     <Copiador textoParaCopiar="https://www.sp.senai.br/">
-                            <div className="w-full max-w-[260px] md:max-w-[299px] h-[50px] bg-[#78C0E5] p-2 mt-3 rounded-lg flex items-center justify-between border-2 border-[#78C0E5] shadow-md cursor-pointer">
+                            <div className="w-full max-w-[260px] md:max-w-[280px] h-[50px] bg-[#78C0E5] p-2 mt-3 rounded-lg flex items-center justify-between border-2 border-[#78C0E5] shadow-md cursor-pointer">
                                 <div className="text-left overflow-hidden">
-                                    <p className="text-xs md:text-xs font-medium">Chave Pix</p>
-                                    <p className="text-sm md:text-sm font-semibold text-[#FFFFFF] truncate">https://www.sp.senai.br/</p>
+                                    <p className="text-[10px] md:text-sm font-medium">Chave Pix</p>
+                                    <p className="text-xs md:text-base font-semibold text-[#FFFFFF] truncate">https://www.sp.senai.br/</p>
                                 </div>
                                 <Copy size={16} className="text-gray-600 flex-shrink-0 ml-2" />
                             </div>
                         </Copiador>
 
-                        <label className={`w-[90%] max-w-[400px] min-h-[160px] mt-3 rounded-sm bg-[#F5F5F5] flex flex-col items-center justify-center cursor-pointer`}
+                        <label className={`w-[90%] max-w-[300px] min-h-[120px] lg:max-w-[400px] lg:min-h-[160px] mt-3 mb-3 rounded-sm bg-[#F5F5F5] flex flex-col items-center justify-center cursor-pointer`}
                             onDragOver={(e) => e.preventDefault()}
                             onDrop={(e) => {
                                 e.preventDefault();
@@ -151,7 +151,7 @@ function EscolhaPlano() {
                         >
                             <input type="file" className="hidden" onChange={(e) => setComprovanteComum(e.target.files?.[0] || null)} />
 
-                            <p className="text-base font-semibold text-[#888888]">
+                            <p className="text-xs md:text-sm lg:text-base font-semibold text-[#888888]">
                                 {comprovanteComum ? comprovanteComum.name : "Arraste o arquivo até aqui!"}
                             </p>
 
@@ -163,24 +163,24 @@ function EscolhaPlano() {
                 <CardPlano
                     titulo="Plano Premium"
                     className="bg-[#1D1D1D] border-4 border-[#F0C41B]"
-                    fundoTitulo="bg-white ml-10.5 mt-10 w-[90%] h-[40px] rounded-l-xs"
+                    fundoTitulo="bg-white ml-10 mt-8 md:ml-12.5 md:mt-9 lg:ml-10.5 lg:mt-10 w-[85%] lg:w-[90%] h-[40px] rounded-l-xs"
                     popular={true}
                     textoBtn="Upload"
                     corBtn="bg-[#86D5FE]"
                     onClick={() => handlPrepararEnvio("premium", comprovantePremium)}
                 > 
-                    <img src="src/assets/images/qrcode.png" alt="qrcode" className="w-[120px] h-[120px] md:w-[187px] md:h-[187px]  rounded-2xl " />
+                    <img src="src/assets/images/qrcode.png" alt="qrcode" className="w-[110px] h-[110px] md:w-[150px] md:h-[150px] lg:w-[187px] lg:h-[187px]  rounded-2xl " />
                     <Copiador textoParaCopiar="https://www.sp.senai.br/">
                             <div className="w-full max-w-[260px] md:max-w-[299px] h-[50px] bg-[#424242] p-2 mt-3 rounded-lg flex items-center justify-between shadow-md cursor-pointer">
                                 <div className="text-left overflow-hidden">
-                                    <p className="text-xs md:text-xs font-medium text-[#F0C72B]">Chave Pix</p>
-                                    <p className="text-sm md:text-sm font-semibold text-[#FFFFFF] truncate">https://www.sp.senai.br/</p>
+                                    <p className="text-[10px] md:text-sm font-medium text-[#F0C72B]">Chave Pix</p>
+                                    <p className="text-xs md:text-base font-semibold text-[#FFFFFF] truncate">https://www.sp.senai.br/</p>
                                 </div>
                                 <Copy size={16} className="text-[#F0C72B] flex-shrink-0 ml-2" />
                             </div>
                         </Copiador>
 
-                        <label className={`w-[90%] max-w-[400px] h-40 mt-3 mb-3 rounded-sm bg-[#F5F5F5] flex flex-col items-center justify-center p-2 cursor-pointer`}
+                        <label className={`w-[90%] max-w-[300px] min-h-[120px] lg:max-w-[400px] lg:min-h-[160px] mt-3 mb-3 rounded-sm bg-[#F5F5F5] flex flex-col items-center justify-center p-2 cursor-pointer`}
                             onDragOver={(e) => e.preventDefault()}
                             onDrop={(e) => {
                                 e.preventDefault();
@@ -190,7 +190,7 @@ function EscolhaPlano() {
                         >
                             <input type="file" className="hidden" onChange={(e) => setComprovantePremium(e.target.files?.[0] || null)} />
 
-                            <p className="text-base font-semibold text-[#888888]">
+                            <p className="text-xs md:text-sm lg:text-base font-semibold text-[#888888]">
                                 {comprovantePremium ? comprovantePremium.name : "Arraste o arquivo até aqui!"}
                             </p>
 
