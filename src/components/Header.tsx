531ee5d48df32196b1943cb8ffbf6944f66f0c7c
NicAlmeida <nicolly.almeida.nicky@gmail.com>
2026-06-01 15:06:47 -0300

    style: adicionando cursor na navegação de página

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -103,12 +103,12 @@ function Header({ usuario, setUsuario }: HeaderProps) {
                         <img
                             src={logoImg}
                             alt="Logo"
-                            className="h-[40px] md:h-[45px] w-auto object-contain"
+                            className="h-[40px] md:h-[45px] w-auto object-contain cursor-pointer"
                             onClick={()=>navigate("/")}
                         />
 
                         <p
-                            className={`text-sm md:text-xl font-semibold whitespace-nowrap transition-colors ${
+                            className={`text-sm md:text-xl font-semibold whitespace-nowrap transition-colors cursor-pointer ${
                                 isOpen
                                     ? "text-black"
                                     : "text-white"
