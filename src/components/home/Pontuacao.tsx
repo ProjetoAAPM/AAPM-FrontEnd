738ebb1c0cc360defc80428469259c93b0db8f58
Leticia-Aymee <leleaymee@gmail.com>
2026-06-11 14:55:26 -0300

    feat: adicionar moedas premium douradas na animação do porquinho

--- src/components/home/Pontuacao.tsx
+++ src/components/home/Pontuacao.tsx
@@ -53,6 +53,7 @@ export default function Pontuacao({
               <PorcoPorcentagem
                 progresso={progresso}
                 animar={animar}
+                isPremium={usuario?.premium}
               />
             </div>
 
--- src/components/home/PorcoPorcentagem.tsx
+++ src/components/home/PorcoPorcentagem.tsx
@@ -1,24 +1,29 @@
 import porquinho from "../../assets/images/Porquinho.png";
 import moeda from "../../assets/images/moeda.png";
+import MoedaPremium from "../../assets/images/MoedaPremium.png";
 
 type Props = {
   progresso?: number;
   animar?: boolean;
+  isPremium?: boolean;
 };
 
 export default function PorcoPorcentagem({
   progresso = 0,
   animar = false,
+  isPremium = false
 }: Props) {
   const percentual = Math.min(Math.max(progresso, 0), 100);
+
+  const ImagemMoeda = isPremium ? MoedaPremium : moeda;
 
   return (
     <div className="relative flex items-center justify-center w-full mt-20 min-[350px]:mt-35 sm:mt-25 md:mt-6 lg:mt-2">
       {animar && (
         <>
-          <img src={moeda} alt="Moeda" className="absolute top-[-80px] left-[46%] w-9 sm:w-12 md:w-14 z-20 moeda-caindo" />
-          <img src={moeda} alt="Moeda" className="absolute top-[-120px] left-[52%] w-8 sm:w-10 md:w-12 z-20 moeda-caindo" style={{ animationDelay: "0.15s" }} />
-          <img src={moeda} alt="Moeda" className="absolute top-[-100px] left-[42%] w-7 sm:w-9 md:w-11 z-20 moeda-caindo" style={{ animationDelay: "0.3s" }} />
+          <img key={`moeda-1-${isPremium}`} src={ImagemMoeda} alt="Moeda" className="absolute top-[-80px] left-[46%] w-9 sm:w-12 md:w-14 z-20 moeda-caindo" />
+          <img key={`moeda-2-${isPremium}`} src={ImagemMoeda} alt="Moeda" className="absolute top-[-120px] left-[52%] w-8 sm:w-10 md:w-12 z-20 moeda-caindo" style={{ animationDelay: "0.15s" }} />
+          <img key={`moeda-3-${isPremium}`}  src={ImagemMoeda} alt="Moeda" className="absolute top-[-100px] left-[42%] w-7 sm:w-9 md:w-11 z-20 moeda-caindo" style={{ animationDelay: "0.3s" }} />
         </>
       )}
 
